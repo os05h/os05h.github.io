ffd2d08f6c1f3e3e03f94c2550a1e2aa97fc19c7
Oskar Hegner <2oskarh@gmail.com>
2022-11-07 23:19:03 +0100

    ENTER

--- program/Shop/index.js
+++ program/Shop/index.js
@@ -5,12 +5,12 @@ function add(kradd, item) {
     kr = kr + kradd;
     var items = cart.length;
     cart[items] = " " + item;
-    document.getElementById("demo").innerHTML = cart;
+    document.getElementById("cart").innerHTML = cart;
     document.getElementById("kr").innerHTML = kr;
 }
 function reset() {
     kr = 0;
     cart.length = 0
     document.getElementById("kr").innerHTML = kr;
-    document.getElementById("demo").innerHTML = cart;
+    document.getElementById("cart").innerHTML = cart;
 }
