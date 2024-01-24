8d5ba525055e4aea0a65731d6440312bcf9f78dd
Oskar Hegner <2oskarh@gmail.com>
2024-01-24 13:09:43 +0100

    stavefejl

--- spil/klike spil/java script.js
+++ spil/klike spil/java script.js
@@ -4,103 +4,103 @@ var upgrades = 1;
 var uplevel = 1;
 
 
-function opensetings(){
-    document.getElementById('shopside').style.display='none';
-    document.getElementById('klikeside').style.display='none';
-    document.getElementById('setingsside').style.display='block'; 
+function opensetings() {
+    document.getElementById('shopside').style.display = 'none';
+    document.getElementById('klikeside').style.display = 'none';
+    document.getElementById('setingsside').style.display = 'block';
 }
 
-function load(){
+function load() {
     alle();
-    document.getElementById('shopside').style.display='none';
-    document.getElementById('klikeside').style.display='none';
-    document.getElementById('setingsside').style.display='none';
+    document.getElementById('shopside').style.display = 'none';
+    document.getElementById('klikeside').style.display = 'none';
+    document.getElementById('setingsside').style.display = 'none';
 }
 
-function alle(){
+function alle() {
 
-    document.getElementById("antalklik").innerHTML = "penge: " + rundop(p); 
+    document.getElementById("antalklik").innerHTML = "Money: " + rundop(p);
 
 
-    document.getElementById("prisup1").innerHTML = "pris: " + rundop(prisupg1()); 
-    document.getElementById("upgrads").innerHTML = "level:" + upgrades + "-" + uplevel;
+    document.getElementById("prisup1").innerHTML = "Pris: " + rundop(prisupg1());
+    document.getElementById("upgrads").innerHTML = "Level:" + upgrades + "-" + uplevel;
 }
 
-function rundop(input){
-    if(input>=1000 && input<1000000){
-        var i = Math.round(input/10)/100;
-        return i + "k"; 
-    }else if(input>=1000000 && input <1000000000){
-        var i = Math.round(input/10000)/100;
-        return i +"M"; 
-    }else if(input>=1000000000 && input <1000000000000){
-        var i = Math.round(input/10000000)/100;
-        return i +"B"; 
-    }else if(input>=1000000000000 && input <1000000000000000){
-        var i = Math.round(input/10000000000)/100;
-        return i +"T"; 
-    }else{
+function rundop(input) {
+    if (input >= 1000 && input < 1000000) {
+        var i = Math.round(input / 10) / 100;
+        return i + "k";
+    } else if (input >= 1000000 && input < 1000000000) {
+        var i = Math.round(input / 10000) / 100;
+        return i + "M";
+    } else if (input >= 1000000000 && input < 1000000000000) {
+        var i = Math.round(input / 10000000) / 100;
+        return i + "B";
+    } else if (input >= 1000000000000 && input < 1000000000000000) {
+        var i = Math.round(input / 10000000000) / 100;
+        return i + "T";
+    } else {
         return input;
     }
- 
+
 }
 
-function onklik(){
+function onklik() {
     p = p + plus;
     alle();
 }
 
-function setings(){
+function setings() {
     document.getElementById("shopside").style.display = "none";
-    document.getElementById('klikeside').style.display='block';
-    document.getElementById('setingsside').style.display='none';
+    document.getElementById('klikeside').style.display = 'block';
+    document.getElementById('setingsside').style.display = 'none';
 }
 
-function openshop(){
+function openshop() {
     document.getElementById("shopside").style.display = "block";
-    document.getElementById('klikeside').style.display='none';
-    document.getElementById('setingsside').style.display='none';
+    document.getElementById('klikeside').style.display = 'none';
+    document.getElementById('setingsside').style.display = 'none';
     alle();
 }
 
-function startgame(){
+function startgame() {
     document.getElementById("klikeside").style.display = "block";
-    document.getElementById('startgame').style.display='none';
-    document.getElementById('setingsside').style.display='none';
+    document.getElementById('startgame').style.display = 'none';
+    document.getElementById('setingsside').style.display = 'none';
     alle();
 }
 
-function back(){
+function back() {
     document.getElementById("shopside").style.display = "none";
-    document.getElementById('klikeside').style.display='block';
-    document.getElementById('setingsside').style.display='none';
+    document.getElementById('klikeside').style.display = 'block';
+    document.getElementById('setingsside').style.display = 'none';
     alle();
 }
 
-function prisupg1(){
-    if(upgrades == 9){
-        return plus*10*upgrades * uplevel * 3;
-    }else{
-        return plus*10*upgrades * uplevel * 1;
+function prisupg1() {
+    if (upgrades == 9) {
+        return plus * 10 * upgrades * uplevel * 3;
+    } else {
+        return plus * 10 * upgrades * uplevel * 1;
     }
 }
 
-function upgrade(){
-    if (upgrades == 9 ){ 
-        if(p >= prisupg1()){
+function upgrade() {
+    if (upgrades == 9) {
+        if (p >= prisupg1()) {
             p = p - prisupg1();
             plus = plus * 10;
             upgrades = 1;
             uplevel++;
-        }else {
-            alert("du har ikke nok penge!!!!!!");
+        } else {
+            alert("You don't have enough money!!!!!!");
         }
-    }else if (p >= prisupg1()){
+    } else if (p >= prisupg1()) {
         p = p - prisupg1();
         plus = plus * 2;
         upgrades++;
-    }else{
-        alert("du har ikke nok penge!!!!!!");
+    } else {
+        alert("You don't have enough money!!!!!!");
     }
     alle();
 }
