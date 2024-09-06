9c921a9fa31da8539806ff6f7fd2f9d8a9d179ae
Oskar Hegner <52626254+os05h@users.noreply.github.com>
2024-09-06 22:15:41 +0000

    chore: Update astro.config.mjs and deploy.yml files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://github.3dgarage.dk',
+    //base: 'my-repo',
+});
